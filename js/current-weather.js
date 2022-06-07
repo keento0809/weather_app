0be60ef7463cd2c80c355f92f82a4140148efa7c
Vini Guimarães <niciusfg@gmail.com>
2022-06-07 10:17:41 -0700

    Current Weather + google api

--- js/current-weather.js
+++ js/current-weather.js
@@ -1,9 +1,23 @@
-async function weatherApi() {
-  const weather = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?lat=49.2846717&lon=-123.1200546&appid=02a1c175ec798f53253a8fd478e1c50c"
-  );
-  const data = await weather.json();
-  console.log(data);
-}
+const submitBtn = document.querySelector(".submitValue")
+const inputValue = document.getElementById('pac-input')
+const cityName = document.querySelector('.cityName')
+const description = document.querySelector('description')
+const temperature = document.querySelector('temperature')
 
-weatherApi();
+
+submitBtn.addEventListener('click', async function(){
+    const weather = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=02a1c175ec798f53253a8fd478e1c50c`
+    );
+    const data = await weather.json();
+    
+    const nameValue = data['name']
+    cityName.innerHTML = nameValue
+
+    console.log(data);
+
+    if(inputValue.value == ""){
+      alert("Please insert a City name!")
+    }
+})
+
